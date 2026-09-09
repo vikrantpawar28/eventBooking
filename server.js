6681f5f0016d0be2f85b6d9d7f6ee0aa0a590983
Vikrant Pawar <111010856+vikrantpawar28@users.noreply.github.com>
2026-09-09 12:37:37 +0530

    Update server.jsvxv

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.get("/", (req, res) => {
+  res.send("Welcome to event booking  Backend");
+});
 
 routeRegistry(app);
 
